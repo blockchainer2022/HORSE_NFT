54fdfd4effe6c78f6ac7c9e9077f1723a0e54b72
coders003 <75733764+Rakeshpradhan1999@users.noreply.github.com>
2021-12-15 01:02:30 +0530

    update

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,7 +3,7 @@ const Index = () => {
     <footer className="text-center py-20 bg-primary-400">
       <div className="container">
         <div className="flex justify-between items-center flex-wrap">
-          <h6>Horse game</h6>
+          <h6 className="text-3xl uppercase">Horse game</h6>
           <div className="grid grid-flow-col gap-4 my-4">
             <div className="h-10 w-10 rounded-full flex items-center justify-center text-xl bg-white text-primary-400 cursor-pointer">
               <i class="fab fa-telegram"></i>
