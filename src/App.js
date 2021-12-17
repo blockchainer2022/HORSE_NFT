af0bb67fecfb761da7807d611bc780d0fe7cb05c
coders003 <91312709+blockchainer2022@users.noreply.github.com>
2021-12-17 13:06:12 +0530

    update config

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ function App() {
     // you are connected to main net
     // Please connect to main net
 
-    if (chainId === 4) {
+    if (chainId === 1) {
       toast(`You are connected to main net`, {
         type: "success",
         position: toast.POSITION.BOTTOM_CENTER,
@@ -134,7 +134,7 @@ function App() {
 
   async function mint(mintCount) {
     if (contract) {
-      if (chainId === 4) {
+      if (chainId === 1) {
         const saleOpen = await contract.methods.presaleOpen().call();
         if (saleOpen) {
           const { data } = await axios.get(
